145d8ac51033114810b3d3a28d1284af238a753c
pranta-barua007 <prantabarua247@gmail.com>
2022-12-01 13:29:03 +0600

    tab tabs container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './user/user.module';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
