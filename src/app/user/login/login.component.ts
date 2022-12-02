f8973e600972c3721f730669fac1bc757ca283d5
pranta-barua007 <prantabarua247@gmail.com>
2022-12-02 22:17:58 +0600

    login form with templateForm

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showAlert = false;
+  alertMsg = 'Please wait! Logging in.';
+  alertColor = 'blue';
+  
+  credentials = {
+    email: '',
+    password: ''
+  }
 
+  login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Logging in.';
+    this.alertColor = 'blue';
+  }
 }
