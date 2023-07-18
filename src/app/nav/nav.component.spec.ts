db769a7e8ecc904be8398f0968a39709439f353e
pranta-barua007 <prantabarua247@gmail.com>
2023-07-18 22:33:14 +0600

    wrote unit testing for about, tab, tabs container, nav component using mocks and spy

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { of } from 'rxjs';
 
 import { NavComponent } from './nav.component';
+import { AuthService } from '../services/auth.service';
+import { Store } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
 
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthService',
+    ['createUser', 'logout'],
+    {
+      isAuthenticated$: of(true), //an observable, always emits true
+    }
+  );
+  const mockedStore = jasmine.createSpyObj('Store', ['dispatch']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [NavComponent],
+      providers: [
+        { provide: AuthService, useValue: mockedAuthService },
+        { provide: Store, useValue: mockedStore },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavComponent);
     component = fixture.componentInstance;
@@ -19,5 +38,20 @@ describe('NavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should log out', () => {
+    const logoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
+
+    expect(logoutLink).withContext('Not logged in').toBeTruthy();
+
+    // simulating a DOM event (triggering a click event)
+    logoutLink.triggerEventHandler('click');
+
+    const service = TestBed.inject(AuthService);
+
+    expect(service.logout)
+      .withContext('Could not click log out link')
+      .toHaveBeenCalledTimes(1);
   });
 });
--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TabComponent } from './tab.component';
+import { By } from '@angular/platform-browser';
 
 describe('TabComponent', () => {
   let component: TabComponent;
@@ -19,5 +20,29 @@ describe('TabComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have .hidden class', () => {
+    // ways to select elems of a component 
+    //way 1 (preferred)
+    const element = fixture.debugElement.query(
+      By.css('.hidden')
+    );
+    //way 2
+    //const element2 = fixture.nativeElement.querySelector('.hidden');
+    //way 3 (not recommended)
+    //const element3 = document.querySelector('.hidden');
+    expect(element).toBeTruthy();
+  });
+
+  it('should not have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(
+      By.css('.hidden')
+    );
+    
+    expect(element).not.toBeTruthy();
   });
 });
